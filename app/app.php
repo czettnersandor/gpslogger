cb5ee8c51557f2eac927869ae98ef61db5180ae7
Sándor Czettner <sandor@czettner.hu>
2016-01-01 20:14:58 +0000

    Tzpo fixes

--- app/app.php
+++ app/app.php
@@ -4,7 +4,7 @@ use \Czettner\GpsLogger\Devices;
 
 $app = require_once __DIR__.'/bootstrap.php';
 
-$app->get('devices', function () use ($app) {
+$app->get('/devices', function () use ($app) {
     $devices = new Devices($app['db']);
     return $app['twig']->render('devices.twig', array(
         'devices' => $devices->getAllDevices(),
@@ -16,7 +16,7 @@ $app->post('/log', function (Request $request) {
     $lat = $request->get('lat');
     $lng = $request->get('lng');
 
-    return new Response('Thank you for your feedback!', 201);
+    return new Response('SUCCESS', 201);
 });
 
 return $app;
