e0ef0e181e7158684edbae08fc8a3d29fa5f1168
Sándor Czettner <sandor@czettner.hu>
2016-01-10 12:45:11 +0000

    Removed duplicates

--- tests/DevicesTest.php
+++ tests/DevicesTest.php
@@ -21,45 +21,4 @@ class DevicesTest extends AbstractWebTestCase
         $this->assertCount(1, $crawler->filter('li:contains("My Galaxy S3")'));
         $this->assertCount(1, $crawler->filter('li > span:contains("mydevicehash")'));
     }
-
-    /**
-     * log
-     */
-    public function testLogPosition()
-    {
-        $client = $this->createClient();
-        $crawler = $client->request(
-            'POST',
-            '/log',
-            [
-                'lat' => 66,
-                'lng' => 88.99,
-                'timestamp' => 123456,
-                'hash' => 'mydevicehash',
-            ]
-        );
-        $this->assertTrue($client->getResponse()->isOk());
-
-        $results = $this->app['db']->fetchAssoc('SELECT * FROM `positions`;');
-        $this->assertCount(5, $results);
-        $this->assertEquals($results['lat'], 66);
-        $this->assertEquals($results['lng'], 88.99);
-    }
-
-    public function testLogDuplicates()
-    {
-        $client = $this->createClient();
-        $logData = [
-            'lat' => 66,
-            'lng' => 88.99,
-            'timestamp' => 123456,
-            'hash' => 'mydevicehash',
-        ];
-        $crawler = $client->request('POST', '/log', $logData);
-        $this->assertTrue($client->getResponse()->isOk());
-        $crawler = $client->request('POST', '/log', $logData);
-        $this->assertFalse($client->getResponse()->isOk());
-        $results = $this->app['db']->fetchAssoc('SELECT * FROM `positions`;');
-        $this->assertCount(5, $results);
-    }
 }
