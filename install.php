06feedde3d8a55bbabe7cf44e81a79a46d65971c
Sándor Czettner <sandor@healthywebsites.co.uk>
2016-01-13 11:59:14 +0000

    Use FLOAT and auto increment primary keys

--- install.php
+++ install.php
@@ -6,14 +6,15 @@
  * version.
  */
 
-// TODO: implement.
+// TODO: use DBAL because AUTOINCREMENT is the syntax in SQLite
+// and AUTO_INCREMENT in MySQL.
 
 if (!isset($app)) {
     $app = require(__DIR__.'/app/app.php');
 }
 
 $sql = "CREATE TABLE IF NOT EXISTS `devices` (
-   `id` INTEGER PRIMARY KEY,
+   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
    `name` TEXT NOT NULL,
    `hash` TEXT NOT NULL
 )
@@ -22,11 +23,11 @@ $sql = "CREATE TABLE IF NOT EXISTS `devices` (
 $app['db']->executeQuery($sql);
 
 $sql = "CREATE TABLE IF NOT EXISTS `positions` (
-   `id` INTEGER PRIMARY KEY,
+   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
    `device_id` INT     NOT NULL,
    `timestamp` INT     NOT NULL,
-   `lat`       INT     NOT NULL,
-   `lng`       INT     NOT NULL
+   `lat`       FLOAT   NOT NULL,
+   `lng`       FLOAT   NOT NULL
 )
 ";
 
