2f708efff725ffe5158e277cc555c878683ea3f5
Sándor Czettner <sandor@czettner.hu>
2016-01-20 21:16:48 +0000

    Changed to Leaflet JS

--- web/map.js
+++ web/map.js
@@ -1,38 +1,24 @@
 Zepto(function($){
 
-    var iconStyle = new ol.style.Style({
-        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-            anchor: [0.5, 46],
-            anchorXUnits: 'fraction',
-            anchorYUnits: 'pixels',
-            opacity: 0.75,
-            src: 'https://www.pragueeventscalendar.com/src/templates/images/web/marker_icon.png'
-        }))
+    var map = L.map('mapdiv').setView([51.5, -0.09], 13);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+
+    var LeafIcon = L.Icon.extend({
+        options: {
+            //shadowUrl: '../docs/images/leaf-shadow.png',
+            iconSize:     [38, 50],
+            shadowSize:   [50, 64],
+            iconAnchor:   [22, 94],
+            shadowAnchor: [4, 62],
+            popupAnchor:  [-3, -76]
+        }
     });
 
-    var vectorSource = new ol.source.Vector({
-        projection: ol.proj.get('EPSG:4326')
-    });
-
-    //add the feature vector to the layer vector, and apply a style to whole layer
-    var vectorLayer = new ol.layer.Vector({
-        source: vectorSource,
-        style: iconStyle
-    });
-
-    var map = new ol.Map({
-        layers: [
-            new ol.layer.Tile({ source: new ol.source.OSM() }),
-            vectorLayer
-        ],
-        view: new ol.View2D({
-            projection: 'EPSG:1234',
-            center: ol.proj.transform(new ol.Coordinate(-111, 45), 'EPSG:4326', 'EPSG:1234'),
-            zoom: 3
-        }),
-        target: 'mapdiv',
-        renderer: 'canvas',
-    });
+    var greenIcon = new LeafIcon({iconUrl: '/map-marker.png'});
 
     // Get last positions
     $.ajax({
@@ -47,40 +33,24 @@ Zepto(function($){
 
         $.each(data, function(key, value){
             console.log(value);
-            points.push([value.lng, value.lat]);
+            points.push([value.lat, value.lng]);
             lastLat = value.lat;
             lastLng = value.lng;
+            L.marker([lastLat, lastLng], {icon: greenIcon}).bindPopup("I am a green leaf.").addTo(map);
         });
 
+        var arrow = L.polyline([[57, -19], [60, -12]], {}).addTo(map);
         console.log(points);
+        // create a red polyline from an array of LatLng points
+        
+        var arrow = L.polyline(points, {}).addTo(map);
+
+        var polyline = L.polyline(points, {color: 'red'}).addTo(map);
+
+        // zoom the map to the polyline
+        map.fitBounds(polyline.getBounds());
 
         // Path
-
-        var points = [
-        [lastLng, lastLat], [0, 0]
-        ];
-
-        lineFeature = new ol.Feature({
-            geometry: new ol.geom.LineString(points),
-            name: 'Line'
-        });
-
-        vectorSource.addFeature(lineFeature);
-
-
-        console.log(lineFeature);
-        // Marker
-
-        var iconFeature = new ol.Feature({
-            geometry: new
-              ol.geom.Point([lastLng, lastLat]),
-            name: 'Null Island ',
-            population: 4000,
-            rainfall: 500
-        });
-        vectorSource.addFeature(iconFeature);
-
-        //map.zoomToExtent(markerLayer.getDataExtent());
       },
       error: function(xhr, type){
         alert('Ajax error!');
